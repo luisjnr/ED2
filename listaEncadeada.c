b5a0852013dc67538c7db1326aaa11a3a20e20f7
luisjnr <luiscarlossantossilvajunior@gmail.com>
2026-04-22 20:49:16 -0300

    up

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -7,22 +7,25 @@
 int interface(){
 	int opc;
 	printf("1 - Adicionar Arquivo\n" "2 - Adicionar Arquivo Prioritário\n"
-	"3 - Listar Arquivos\n" "4 - Listar Primeiro Arquivo da Fila\n" "5 - Excluir Arquivo\n");
+	"3 - Listar Arquivos\n" "4 - Imprimir Arquivo\n" "5 - Excluir Arquivo\n");
 	scanf(" %d", &opc);
 	while(getchar() != '\n');
 	system("clear");
 	return opc;
 }
 
-Document cadastrarDocument(Document tail){
-	char nome[1000];
-	int paginas, tamanho;
-	printf("===== NEW ARQUIVO =====\n\n" 
-	"Nome: ");
-	scanf(" %[^\n]s", nome);
-	paginas = 1 + rand() % 50;
-	tamanho = 100 + rand() % 900;
-	return new_Document(tail, nome, paginas, tamanho);
+Document cadastrarDocument(Document head, Document tail, int flag){
+		char nome[1000];
+		int paginas, tamanho;
+		printf("===== NEW ARQUIVO =====\n\n" 
+		"Nome: ");
+		scanf(" %[^\n]s", nome);
+		paginas = 1 + rand() % 50;
+		tamanho = 100 + rand() % 900;
+	if(!flag)
+		return new_Document(tail, nome, paginas, tamanho);
+	else
+		return new_DocumentPriority(head, nome, paginas, tamanho);
 }
 
 int main(){
@@ -31,22 +34,27 @@ int main(){
 	Document tail = NULL;
 	do{
 		switch (interface()){
-			case 1: tail = cadastrarDocument(tail);
+			case 1: tail = cadastrarDocument(head, tail, 0);
 				if(!head)
 					head = tail;
 				break;
 				
-			case 2:
+			case 2: head = cadastrarDocument(head, tail, 1);
+				if(!tail)
+					tail = head;
 				break;
 				
 			case 3: listar_Documents(head); 
 				break;
 				
-			case 4:
+			case 4: head = imprimir_Arquivo(head);
+				if(!head)
+					tail = NULL;
 				break;
 				
-			case 5:
+			case 5: deletar_Document(&head, &tail);
 				break;
+				
 			default: alert("Erro!\nOpção não existe.\nTente novamente...\n\n");
 		}
 	} while (1);
--- listaEncadeada.h
+++ listaEncadeada.h
@@ -18,7 +18,7 @@ void alert(char frase[]){
 
 Document new_Document(Document tail, char nome[], int paginas, int tamanho){
 	Document novo = malloc(sizeof(Document));
-	novo->nome = malloc(strlen(nome));
+	novo->nome = malloc(strlen(nome)+1);
 	strcpy(novo->nome, nome);
 	novo->paginas = paginas;
 	novo->tamanho = tamanho;
@@ -26,6 +26,20 @@ Document new_Document(Document tail, char nome[], int paginas, int tamanho){
 	if(!tail)
 		return novo;
 	tail->next = novo;
+	return novo;
+}
+
+Document new_DocumentPriority(Document head, char nome[], int paginas, int tamanho){
+	Document novo = malloc(sizeof(Document));
+	novo->nome = malloc(strlen(nome)+1);
+	strcpy(novo->nome, nome);
+	novo->paginas = paginas;
+	novo->tamanho = tamanho;
+	if(!head){
+		novo->next = NULL;
+		return novo;
+	}
+	novo->next = head;
 	return novo;
 }
 
@@ -41,4 +55,67 @@ void listar_Documents(Document head){
 		printf("Tamanho: %d Bytes\n\n", head->tamanho);
 		head = head->next;
 	}
+}
+
+Document imprimir_Arquivo(Document head){
+	if(!head){
+		alert("Erro!\nNenhum arquivo foi cadastrado.\nTente novamente...\n");
+		return head;
+	}
+	printf("===== Impressão =====\n\n");
+	printf("Nome: %s\n", head->nome);
+	printf("Páginas: %d\n", head->paginas);
+	printf("Tamanho: %d Bytes\n", head->tamanho);
+	Document temp = head;
+	free(temp);
+	return head->next;
+}
+
+void selecionar_Alvo(Document head, char alvo[]){
+	do{
+		listar_Documents(head);
+		Document temphead = head;
+		printf("Digite o nome do arquivo: ");
+		scanf(" %s", alvo);
+		while(temphead){
+			if(!strcmp(temphead->nome, alvo))
+				return;
+			temphead = temphead->next;
+		}
+		alert("Erro!\nArquivo não foi cadastrado.\nTente novamente...\n");
+	} while (1);
+}
+
+void deletar_Document(Document* head, Document* tail){
+	if(!(*head)){
+		alert("Erro!\nNenhum arquivo foi cadastrado.\nTente novamente...\n");
+		return;
+	}
+	char alvo[1000];
+	selecionar_Alvo(*head, alvo);
+	if(!strcmp((*head)->nome, alvo)){
+		Document temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+		if(!(*head))
+			(*tail) = NULL;
+		return;
+	}
+	Document headTemp = *head;
+	while(headTemp){
+		if(!strcmp(headTemp->next->nome, alvo)){
+			Document temp = headTemp->next;
+			if(headTemp->next == (*tail)){
+				headTemp->next = NULL;
+				(*tail) = headTemp;
+				free(temp);
+				return;
+			}
+			headTemp->next = temp->next;
+			free(temp);
+			return;
+		}
+		headTemp = headTemp->next;
+	}
+	
 }
